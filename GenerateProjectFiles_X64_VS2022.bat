f83822533e213ae7a3d7694657e24f96b4d4610d
brendanwalker <brendan@millerwalker.net>
2026-05-11 22:03:09 -0700

    Fixing build errors

--- GenerateProjectFiles_X64_VS2022.bat
+++ GenerateProjectFiles_X64_VS2022.bat
@@ -5,7 +5,7 @@ pushd build
 
 echo "Rebuilding Kodgen x64 Project files..."
 
-cmake .. -G "Visual Studio 17 2022" -A x64
+cmake .. -G "Visual Studio 17 2022" -DBUILD_TESTING=OFF -A x64
 IF %ERRORLEVEL% NEQ 0 (
   echo "Error generating Kodgen 64-bit project files"
   goto failure
