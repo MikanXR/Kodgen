4376965566e347e671b87d79e99ea41fd598d416
brendanwalker <brendan@millerwalker.net>
2026-05-11 21:19:46 -0700

    Updating to Clang 19.1.7

---
+++ GenerateProjectFiles_X64_VS2022.bat
@@ -0,0 +1,19 @@
+@echo on
+
+IF NOT EXIST build mkdir build
+pushd build
+
+echo "Rebuilding Kodgen x64 Project files..."
+
+cmake .. -G "Visual Studio 17 2022" -A x64
+IF %ERRORLEVEL% NEQ 0 (
+  echo "Error generating Kodgen 64-bit project files"
+  goto failure
+)
+
+popd
+EXIT \B 0
+
+:failure
+pause
+EXIT \B 1
